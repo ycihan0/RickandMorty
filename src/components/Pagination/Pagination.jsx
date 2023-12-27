13bd1f165e6aa6ac6c228a14f83e86e48a49fce0
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2023-12-27 20:21:41 +0300

    pagination added

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,28 @@
+import ReactPaginate from "react-paginate"
+import "./Pagination.css"
+
+const Pagination = ({ info, pageNumber, setPageNumber }) => {
+
+    return (
+        <ReactPaginate
+            breakLabel="..."
+            nextLabel="Next"
+            // forcePage={pageNumber===1? 0:pageNumber-1}
+            onPageChange={(data) => {
+                setPageNumber(data.selected + 1)
+            }}
+            // pageRangeDisplayed={5}
+            pageCount={info.pages}
+            previousLabel="Prev"
+            containerClassName="pagination-container"
+            pageClassName="pagination-page"
+            breakClassName="pagination-break"
+            previousClassName="pagination-previous"
+            nextClassName="pagination-next"
+            activeClassName="pagination-active"
+
+        />
+    )
+}
+
+export default Pagination
--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -3,12 +3,15 @@ import CharacterItems from "./CharacterItems";
 import { useCallback } from "react";
 import { useEffect } from "react";
 import Card from "../components/UI/Card";
+import Pagination from "../components/Pagination/Pagination";
 
 
 const Characters = () => {
   const [characters, setCharacters] = useState([]);
   const [isLoading, setIsloading] = useState(false);
   const [error, setError] = useState(null);
+  const [info, setInfo] = useState({});
+const [pageNumber, setPageNumber]=useState(1);
 
   const characterList = characters.map((character) => (
       <CharacterItems key={character.id} character={character} />
@@ -18,26 +21,26 @@ const Characters = () => {
     setIsloading(true);
     setError(null);
     try {
-      const response = await fetch("https://rickandmortyapi.com/api/character");
+      const response = await fetch(`https://rickandmortyapi.com/api/character/?page=${pageNumber}`);
       if (response.status !== 200) {
         throw new Error("Something went wrong!");
       }
       const data = await response.json();
       const { results, info } = data;
       setCharacters(results);
+      setInfo(info);
     } catch (error) {
       setError(error.message);
      
     }
     setIsloading(false);
-  });
+  },[pageNumber]);
 
   useEffect(() => {
     fetchCharactersHandler();
    
-  }, [ ]);
+  }, [fetchCharactersHandler]);
  
-
   let content = <p>Found no products!</p>;
 
   if (characters.length > 0) {
@@ -51,11 +54,10 @@ const Characters = () => {
   if (isLoading) {
     content = <p>Loading...</p>;
   }
-
- 
   return (<>
 
   <Card>{content}</Card>
+  <Pagination info={info} pageNumber={pageNumber} setPageNumber={setPageNumber}/>
   </>
     
   );
